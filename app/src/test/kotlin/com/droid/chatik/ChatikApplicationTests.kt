7b04cd22f78d272e43b6d28178db47b4a803add9
danil <danilkikaoskol@mail.ru>
2026-07-16 16:47:57 +0300

    TASK-2 created app module and moved src here

---
+++ app/src/test/kotlin/com/droid/chatik/ChatikApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.droid.chatik
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatikApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
