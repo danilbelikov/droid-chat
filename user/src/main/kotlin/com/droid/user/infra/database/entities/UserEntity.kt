f129465fec85ec881d52d77771f6f52fa347d9b6
danil <danilkikaoskol@mail.ru>
2026-07-18 23:51:51 +0300

    TASK-9 created the User Database Entity

---
+++ user/src/main/kotlin/com/droid/user/infra/database/entities/UserEntity.kt
@@ -0,0 +1,48 @@
+package com.droid.user.infra.database.entities
+
+import com.droid.user.domain.model.UserId
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+
+//не используем data class, так как для java изначально делали jpa
+
+@Entity
+@Table(
+    name = "users",
+    schema = "user_service",
+    indexes = [
+        Index(name = "idx_users_email", columnList = "email"),
+        Index(name = "idx_users_username", columnList = "username"),
+    ]
+)
+class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UserId? = null,
+
+    @Column(nullable = false, unique = true)
+    var email: String,
+
+    @Column(nullable = false, unique = true)
+    var username: String,
+
+    @Column(nullable = false)
+    var hashedPassword: String,
+
+    @Column(nullable = false)
+    var hasVerifiedEmail: Boolean,
+
+    @CreationTimestamp
+    var createdAt: Instant = Instant.now(),
+
+    @UpdateTimestamp
+    var updatedAt: Instant = Instant.now()
+)
